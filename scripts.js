c364a457991ee08accc916f8305c81d484d46e52
Toby Kerslake <kerslake.tobias@gmail.com>
2018-02-21 23:01:34 +0000

    Added javascript/Jquery for fading in title text

---
+++ scripts.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function(){
+    fadeInText();
+});
+
+//function for fading in title text 
+function fadeInText(){
+	
+	$(".logo").fadeIn(2000);
+	$("nav").fadeIn(2000);
+	$(".title").delay(2000).fadeIn(2000);
+	$(".kicker").delay(3000).fadeIn(2000);
+	}
+  
